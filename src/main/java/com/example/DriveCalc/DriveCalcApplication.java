98f06ecf67caf0c7a490639ef86f4f3c6fb42686
Cristian Furtuna <cristianfurtuna840@gmail.com>
2023-11-22 14:35:24 +0200

    First commit

---
+++ src/main/java/com/example/DriveCalc/DriveCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.DriveCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DriveCalc/DriveCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DriveCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
